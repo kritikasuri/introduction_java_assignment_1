8f5f9edbe348d2899544e2acfe7145ccc7c9d806
kritikasuri <surikritika@gmail.com>
2016-05-14 20:54:27 +0530

    Add files via upload

---
+++ Assignment_1.java
@@ -0,0 +1,13 @@
+
+public class sumOf2_nbrs {
+	public static void main(String[] args) {
+		
+		int n1=10;
+		float n2=(float) 10.23;
+		int sum = 0;
+		
+		sum = n1+ (int)n2;
+		System.out.println("The sum 2 numbers is : "+ sum);		
+		
+	}
+}
